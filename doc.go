d9914f9273c7940b4cb309b4e2b7b29de5d61435
Vincenzo Di Somma <vincenzo.disomma@gmail.com>
2015-01-26 16:07:58 +0100

    adding package documentation.

---
+++ doc.go
@@ -0,0 +1,21 @@
+/*
+
+Overview
+
+Package oops, inspired by the OOPS tool of launchpad.net (https://launchpad.net/python-oops), is simpler, less bloated, go native implementation of oops reports.
+An OOPS is an error report that a developer or a system administrator can access easily and quickly.
+Oopses are stored locally as files. Once recorded they can be moved to a central service that indexes them and provide a convenient interface to search and browse them, refer to the oopsio package for a simple implementation of such service.
+
+Example
+
+	err := errors.New("this is an error")
+	tempFolder, err := ioutil.TempDir("/tmp", "oops")
+	p := publishers.DiskPublisher{tempFolder}
+	of := factory.OopsFactory{p}
+	id := of.NewOops(e, true)
+	// Log that an Oops was recorded, together with the Oops id
+	log.Printf("Oops! Oops id: %s\n", id)
+
+*/
+
+package oops
